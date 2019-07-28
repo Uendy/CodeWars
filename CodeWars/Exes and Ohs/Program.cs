0f319d1fdb7f608bf98485cabfab5527e3153426
Uendy <alex.beshkov98@gmail.com>
2019-07-28 17:59:29 +0300

    Adding Q: Exes and Ohs

---
+++ CodeWars/Exes and Ohs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        //Check to see if a string has the same amount of 'x's and 'o's. 
+        //The method must return a boolean and be case insensitive. 
+        //The string can contain any char.
+
+        //Examples input/ output:
+        //XO("ooxx") => true
+        //XO("xooxx") => false
+        //XO("ooxXm") => true
+        //XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+        //XO("zzoo") => false
+        #endregion
+
+        string input = Console.ReadLine().ToLower();
+
+        var oS = input.ToCharArray().Where(x => x == 'o').ToArray();
+        var xS = input.ToCharArray().Where(x => x == 'x').ToArray();
+
+        Console.WriteLine(oS.Count() == xS.Count());
+
+        //Clever Solution:
+        //return input.ToLower().Count(i => i == 'x') == input.ToLower().Count(i => i == 'o');
+    }
+}
---
+++ CodeWars/Exes and Ohs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Exes and Ohs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("Exes and Ohs")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bce29100-9c11-42c7-9d26-9ebf16d0f1b7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
