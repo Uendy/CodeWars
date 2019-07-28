0a620ab77b5d6c78a4db6e3c87c71160126a6779
Aleksandar Beshkov <35635025+Uendy@users.noreply.github.com>
2019-07-28 14:39:22 +0300

    Add files via upload
    Adding: 
    - CodeWars  Folder
    - Q: Sum of Numbers
    - Q: Your Order, Please

---
+++ CodeWars/Sum Of Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public class Sum
+{
+    #region
+    //Given two integers a and b, which can be positive or negative,
+    //find the sum of all the numbers between including them too and return it.
+    //If the two numbers are equal return a or b.
+    //Note: a and b are not ordered!
+
+    //Examples
+    //GetSum(1, 0) == 1   // 1 + 0 = 1
+    //GetSum(1, 2) == 3   // 1 + 2 = 3
+    //GetSum(0, 1) == 1   // 0 + 1 = 1
+    //GetSum(1, 1) == 1   // 1 Since both are same
+    //GetSum(-1, 0) == -1 // -1 + 0 = -1
+    //GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+    #endregion
+
+    public static void Main()
+    {
+        Console.WriteLine(GetSum(3, 3));
+    }
+
+    public static int GetSum(int a, int b)
+    {
+        int top = Math.Max(a, b);
+        int bot = Math.Min(a, b);
+
+        int sum = 0;
+        for (int i = bot; i <= top; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
---
+++ CodeWars/Your Order, Please/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+public static class Kata
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+
+        var inputTokens = input.Split(' ').ToArray();
+
+        int numberOfWords = inputTokens.Count();
+
+        var outPutArray = new string[numberOfWords + 1];
+
+        foreach (var word in inputTokens)
+        {
+            var wordAsArray = word.ToCharArray();
+
+            int position = -1;
+
+            foreach (char symbol in wordAsArray)
+            {
+                bool numberFound = int.TryParse(symbol.ToString(), out position);
+                if (numberFound)
+                {
+                    outPutArray[position] = word;
+                    break;
+                }
+            }
+        }
+
+        var outPut = string.Join(" ", outPutArray);
+        Console.WriteLine(outPut.TrimStart());
+    }
+}
