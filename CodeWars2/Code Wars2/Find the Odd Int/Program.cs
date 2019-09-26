971ecb8e761cc06c396dd29b89e0ed3c59cac466
Uendy <alex.beshkov98@gmail.com>
2019-09-26 13:41:23 +0300

    Adding: -Kata: Find the Odd Int

---
+++ CodeWars2/Code Wars2/Find the Odd Int/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        int[] seq = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+        int oddNum = FindOddOccuranceInt(seq);
+
+        Console.WriteLine(oddNum);
+    }
+
+    public static int FindOddOccuranceInt(int[] seq)
+    {
+        return seq.ToList()
+                  .GroupBy(x => x) //Group by each element in the array
+                  .Where(x => (x.Count() % 2) != 0) //Find grouped odd numbers
+                  .Select(x => x.First())
+                  .FirstOrDefault(); //since array will only contain 1 odd number, get first one
+    }
+}
