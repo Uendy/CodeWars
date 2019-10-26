f5583c084655d068e1720222eae7158591245dd0
Uendy <alex.beshkov98@gmail.com>
2019-10-26 18:17:35 +0300

    Adding: -Kata: Distinct Year

---
+++ CodeWars2/Code Wars2/Distinct Year/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //The year of 2013 is the first year after the old 1987 with only distinct digits.
+
+        //Now your task is to solve the following problem: given a year number, 
+        //find the minimum year number which is strictly larger than the given one and has only distinct digits.
+
+        int year = int.Parse(Console.ReadLine());
+
+        var distinctYear = FindNextDistinctYear(year);
+
+        Console.WriteLine(distinctYear);
+    }
+
+    public static object FindNextDistinctYear(int year)
+    {
+        while (true)
+        {
+            ++year;
+
+            string yearAsString = year.ToString();
+            int distinctNumbers = yearAsString.Distinct().Count();
+            if (distinctNumbers == 4)
+            {
+                return year;
+            }
+
+        }
+
+    }
+}
---
+++ CodeWars2/Code Wars2/Range Bit Counting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //You are given two numbers a and b where 0 ≤ a ≤ b. 
+        //Imagine you construct an array of all the integers from a to b inclusive. 
+        //You need to count the number of 1s in the binary representations of all the numbers in the array.
+
+        //Example
+        //For a = 2 and b = 7, the output should be 11
+
+        //Given a = 2 and b = 7 the array is: [2, 3, 4, 5, 6, 7]. 
+        //Converting the numbers to binary, we get[10, 11, 100, 101, 110, 111], which contains 1 + 2 + 1 + 2 + 2 + 3 = 11 1s.
+
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+
+        int numberOfOnes = FindBinaryOnesCount(a, b);
+        Console.WriteLine(numberOfOnes);
+    }
+
+    public static int FindBinaryOnesCount(int a, int b)
+    {
+        int ones = 0;
+
+        for (int i = a; i <= b; i++)
+        {
+            string binary = Convert.ToString(i, 2);
+
+            ones += binary.Where(x => x == '1').Count();
+        }
+
+        return ones;
+    }
+}
