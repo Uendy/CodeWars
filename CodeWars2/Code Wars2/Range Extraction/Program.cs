46c2484a5fd9f773229f5d5dd53c0d5539c459d3
Uendy <alex.beshkov98@gmail.com>
2019-10-29 11:44:33 +0200

    Update: -Kata: Range Extraction

--- CodeWars2/Code Wars2/Range Extraction/Program.cs
+++ CodeWars2/Code Wars2/Range Extraction/Program.cs
@@ -15,5 +15,8 @@ public class Program
         //Example:
         //solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
         // returns "-6,-3-1,3-5,7-11,14,15,17-20"
+
+        //so all the ones that have more than 1 between them return as the same ex: -5, -2, 0 -> -5, -2, 0
+        //all those that are subsequent insert into a range ex: -3, -2, -1, 0, 1 -> -3-1
     }
 }
