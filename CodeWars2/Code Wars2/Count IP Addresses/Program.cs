a99a3a26a313cc3a37487e8a2b2932b1efa5c237
Uendy <alex.beshkov98@gmail.com>
2019-11-06 22:50:42 +0200

    Adding: -Kata: New Cashier

---
+++ CodeWars2/Code Wars2/Count IP Addresses/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Implement a function that receives two IPv4 addresses
+        //return the number of addresses between them (including the first one, excluding the last one).
+
+        //All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+
+        //Examples
+        //ips_between("10.0.0.0", "10.0.0.50") == 50
+        //ips_between("10.0.0.0", "10.0.1.0") == 256
+        //ips_between("20.0.0.10", "20.0.1.0") == 246
+
+        string firstIP = Console.ReadLine();
+        string secondIP = Console.ReadLine();
+
+        long distanceBetween = IpsBetween(firstIP, secondIP);
+
+        Console.WriteLine(distanceBetween);
+    }
+
+    public static long IpsBetween(string firstIP, string secondIP)
+    {
+        var ip1Array = firstIP.Split('.').Select(x => byte.Parse(x)).ToArray();
+        var ip2Array = secondIP.Split('.').Select(x => byte.Parse(x)).ToArray();
+        long result = 0;
+        result += (ip2Array[0] - ip1Array[0]) * 256 * 256 * 256;
+        result += (ip2Array[1] - ip1Array[1]) * 256 * 256;
+        result += (ip2Array[2] - ip1Array[2]) * 256;
+        result += (ip2Array[3] - ip1Array[3]);
+        return result;
+    }
+}
---
+++ CodeWars2/Code Wars2/New Cashier/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        #region
+        //New Cashier Does Not Know About Space or Shift
+
+        //Some new cashiers started to work at your restaurant.
+
+        //They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+
+        //All the orders they create look something like this:
+
+        //"milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+        //The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+
+        //Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+
+        //"Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+
+        //The kitchen staff expect the items to be in the same order as they appear in the menu.
+
+        //The menu items are fairly simple, there is no overlap in the names of the items:
+
+        //1.Burger
+        //2.Fries
+        //3.Chicken
+        //4.Pizza
+        //5.Sandwich
+        //6.Onionrings
+        //7.Milkshake
+        //8.Coke
+        #endregion
+
+        string input = Console.ReadLine();
+
+        string output = GetOrder(input);
+        Console.WriteLine(output);
+    }
+
+    public static string GetOrder(string input)
+    {
+        var menu = new List<string>() { "Burger", "Fries", "Chicken", "Pizza", "Sandwich", "Onionrings", "Milkshake", "Coke" };
+
+        var listOfOrder = new List<string>();
+        string order = string.Join(", ", listOfOrder);
+        return order;
+    }
+}
