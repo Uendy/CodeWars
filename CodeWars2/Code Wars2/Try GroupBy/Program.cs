9786971b27a56d49571fa957ebfcd34b03495352
Uendy <alex.beshkov98@gmail.com>
2019-11-12 11:10:28 +0200

    Adding: -Kata: Try GroupBy

---
+++ CodeWars2/Code Wars2/Try GroupBy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //Write a function that takes an array and counts the number of each unique element present.
+
+        var list = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        foreach (var line in list.GroupBy(x => x)
+                        .Select(group => new
+                        {
+                            name = group.Key,
+                            Count = group.Count()
+                        }))
+        {
+            Console.WriteLine($"{line.name} - {line.Count}");
+        }
+    }
+}
