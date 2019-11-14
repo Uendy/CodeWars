c718e7272a8f90e22cb2b0125954939d1d496605
Uendy <alex.beshkov98@gmail.com>
2019-11-14 22:10:40 +0200

    Update: -Kata: BEDMAS Calculator

--- CodeWars3/BEDMAS Calculator/Program.cs
+++ CodeWars3/BEDMAS Calculator/Program.cs
@@ -2,14 +2,87 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace BEDMAS_Calculator
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        //iven a string of characters and symbols, calculate the expected result. The string consists of numbers, and the operators:
+
+        /// division
+        //+ addition
+        //- subtraction
+        //\*multiplication
+        //^ power of
+        //As well as the brackets()
+
+        //Numbers can be integers or doubles.
+
+        //Assume the string is of the correct format(no missing brackets, unmatched operators). 
+        //The format of the string can also have optional whitespace between numbers and symbols, so the following are equivalent:
+
+        //"3+4*2"
+        //"3 +                             4*   2"
+
+        string input = Console.ReadLine().Trim();
+
+        var result = ApplyCalculator(input);
+
+        Console.WriteLine(result);
+        
+    }
+
+    public static object ApplyCalculator(string input)
+    {
+        var result = 0;
+
+
+        var indexOfBracket = input.IndexOf('(');
+        while (indexOfBracket != -1) // cycle through all brackets and solve them
         {
+
+            int indexOfClosedBracket = input.IndexOf(')');
+            int bracketSpan = indexOfClosedBracket - indexOfBracket;
+
+            var range = input.Substring(indexOfBracket, bracketSpan);
+
+            var operatorsInBrackets = GetOperators(range);
+            var numbersInBrackets = GetNumber(range);
+
+            var newRange = string.Empty; // fill this with the uncovered brackets
+
+            //When you make the methods for each operator fill them in here for whats in the brackets
+
+            input = input.Replace(range, newRange);
+            indexOfBracket = input.IndexOf('('); // check for more brackets, I wont cover double brackets
         }
+
+        return result;
+    }
+
+    public static List<double> GetNumber(string input)
+    {
+        var numbers = input
+            .Split(new[] { '+', '-', '/', '*', '^', '(', ')' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Trim())
+            .Select(double.Parse)
+            .ToList();
+
+        return numbers;
+    }
+
+    public static List<char> GetOperators(string input)
+    {
+        var operators = new List<char>();
+        var inputAsArray = input.ToCharArray();
+        foreach (var item in inputAsArray)
+        {
+            bool isOperator = item == '+' || item == '-' || item == '/' || item == '*' || item == '^' || item == '(' || item == ')';
+            if (isOperator)
+            {
+                operators.Add(item);
+            }
+        }
+
+        return operators;
     }
 }
