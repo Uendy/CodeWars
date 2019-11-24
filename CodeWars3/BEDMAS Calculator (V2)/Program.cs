b24d19b4586779a239ff3afec04b78ee711183a7
Uendy <alex.beshkov98@gmail.com>
2019-11-24 19:55:53 +0200

    Update: -Kata: BEDMAS Calculator (V2)

--- CodeWars3/BEDMAS Calculator (V2)/Program.cs
+++ CodeWars3/BEDMAS Calculator (V2)/Program.cs
@@ -94,6 +94,59 @@ public class Program
 
     public static List<string> ExpandBrackets(List<string> input)
     {
+        //Find all ranges and gets the smallest one, the shortest distance from "(" to ")" and starts with it
+        var indexOfStartBrackets = new List<int>(); // get all the '(' by index
+        int indexOfStart = input.IndexOf("(");
+        while (indexOfStart >= 0)
+        {
+            indexOfStartBrackets.Add(indexOfStart);
+            indexOfStart = input.IndexOf("(", indexOfStart + 1);
+        }
+
+        var indexOfEndBrackets = new List<int>(); // get all the ')' by index
+        int indexOfEnd = input.IndexOf(")");
+        while (indexOfEnd >= 0)
+        {
+            indexOfEndBrackets.Add(indexOfEnd);
+            indexOfEnd = input.IndexOf(")", indexOfEnd + 1);
+        }
+
+        int smallestDifference = int.MaxValue;
+        int smallestStartIndex = int.MaxValue;
+
+        for (int startBracket = 0; startBracket < indexOfStartBrackets.Count(); startBracket++) // find the smallest distance between to brackets
+        {
+            int startIndex = indexOfStartBrackets[startBracket];
+
+            for (int endBracket = 0; endBracket < indexOfEndBrackets.Count(); endBracket++)
+            {
+                int endIndex = indexOfEndBrackets[endBracket];
+
+                int difference = endIndex - startIndex;
+
+                bool shortestDifference = difference < smallestDifference;
+                if (shortestDifference)
+                {
+                    smallestDifference = difference;
+                    smallestStartIndex = startIndex;
+                }
+            }
+        }
+
+        var range = input.GetRange(smallestStartIndex, smallestDifference);
+        //expand the brackets
+        var newRange = BEDMAS(range);
+
+        // make range into a string
+
+        //fix the input by removing old and putting in new
+        input.RemoveRange(smallestStartIndex, smallestDifference);
+        input.InsertRange(smallestStartIndex, newRange);
+        
+        
+
+
+        //return range;
         //find the range and remove it
         // calculate 
         //repeat
@@ -101,32 +154,32 @@ public class Program
         return input;
     }
 
-    //public static List<string> GetNumber(string input)
-    //{
-    //    var numbers = new List<string>();
+    public static List<string> BEDMAS(List<string> range)
+    {
+        var outPut = new List<string>();
 
-    //    string regexPattern = @"[-]?\d+(\.\d+)?"; // took me ages to figure this out
-    //    var regex = new Regex(regexPattern);
+        var numbers = GetNumbers(range);
+        var operators = GetOperators();
 
-    //    var matches = regex.Matches(input);
-    //    foreach (Match match in matches)
-    //    {
-    //        numbers.Add(match.ToString());
-    //    }
+    }
 
-    //    return numbers;
-    //}
-    //public static Dictionary<double, int> GetNumbersAndIndex(string input, List<string> numbers)
-    //{
-    //    var numsAndIndex = new Dictionary<double, int>();
+    public static List<string> GetOperators(List<string> range)
+    {
+        var operators = new List<string>();
 
-    //    foreach (var num in numbers)
-    //    {
-    //        input.IndexOf()
-    //    }
+        var possibleOperators = new List<string>() { "+", "-", "*", "/", "^" };
 
-    //    return numsAndIndex;
-    //}
+        foreach (var item in range)
+        {
+            bool isOperator = possibleOperators.Contains(item);
+            operators.Add(item);
+        }
 
+        return operators;
+    }
 
+    public static List<double>GetNumbers(List<string> range)
+    {
+        throw new NotImplementedException();
+    }
 }
