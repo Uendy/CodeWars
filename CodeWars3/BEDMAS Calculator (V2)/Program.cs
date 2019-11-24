6990d62d115f10382490d3e383e3a71b5433ee68
Uendy <alex.beshkov98@gmail.com>
2019-11-24 18:33:22 +0200

    Adding: -Kata: BEDMAS Calculator (V2) (initial)

---
+++ CodeWars3/BEDMAS Calculator (V2)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main()
+    {
+        string input = Console.ReadLine();
+
+        var formattedInput = SpaceInput(input);
+    }
+
+    public static string SpaceInput(string input) // get all the parts to be spaced with a single inbetween
+    {
+        bool containsMoreSpaces = input.Contains("  "); // gets all multiple spaces into singles
+        while (containsMoreSpaces)
+        {
+            input.Replace("  ", " ");
+            containsMoreSpaces = input.Contains("  ");
+        }
+
+        var operators = new List<char>() { '+', '*', '/', '^' }; // missing '.' and '-'
+        var sb = new StringBuilder();
+
+        var inputAsArray = input.ToCharArray();
+        for (int index = 0; index < inputAsArray.Count(); index++)
+        {
+            char currentChar = inputAsArray[index];
+
+
+        }
+
+        var result = sb.ToString();
+        return result;
+    }
+}
