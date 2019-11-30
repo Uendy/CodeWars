2ed7bf00d29332f5304dd4b091abd23d7fc7965d
Uendy <alex.beshkov98@gmail.com>
2019-11-30 15:49:37 +0200

    Update: -Kata: BEDMAS CALCULATOR (V3)

--- CodeWars3/BEDMAS CALCULATOR (V3)/Program.cs
+++ CodeWars3/BEDMAS CALCULATOR (V3)/Program.cs
@@ -9,9 +9,13 @@ public class Program
     {
         string input = Console.ReadLine();
         var list = GetElements(input);
+        list = ExpandBrackets(list);
 
-        var result = ApplyCalculator(list);
+
+        //var result = ApplyCalculator(list);
     }
+
+    
 
     public static List<string> GetElements(string input) // returns all operators and numbers into a list with correct order
     {
@@ -59,8 +63,121 @@ public class Program
 
         return list;
     }
-    public static double ApplyCalculator(List<string> list)
+    public static List<string> ExpandBrackets(List<string> list)
     {
-        
+        //get all the "("
+        var startBrackets = new List<int>();
+        int indexOfStart = list.IndexOf("(");
+        while (indexOfStart != -1)
+        {
+            startBrackets.Add(indexOfStart);
+        }
+
+        //get all the ")"
+        var endBrackets = new List<int>();
+        int indexOfEnd = list.IndexOf(")");
+        while (indexOfEnd != -1)
+        {
+            endBrackets.Add(indexOfEnd);
+        }
+
+        //find the innerMost Brackets and start from there
+
+        while (startBrackets.Count() != 0)
+        {
+
+            int shortestDistance = int.MaxValue;
+            int indexToStart = 0;
+
+            foreach (var startIndex in startBrackets)
+            {
+                foreach (var endIndex in endBrackets)
+                {
+                    int distance = endIndex - startIndex;
+                    bool newShorterDistance = shortestDistance > distance;
+                    {
+                        shortestDistance = distance;
+                        indexOfStart = startIndex;
+                    }
+                }
+            }
+
+            var range = list.GetRange(indexOfStart, shortestDistance);
+            var result = EDMAS(range);
+
+            //remove the past range with the result
+            list.RemoveRange(indexOfStart, shortestDistance);
+            list.Insert(indexOfStart, result);
+        }
+
+        return list;
+    }
+
+
+    //public static double ApplyCalculator(List<string> list)
+    //{
+
+    //}
+    public static double EDMAS(List<string> range)
+    {
+        //so as to keep any leading minuses in tact
+        range.Insert(0, "+"); 
+        range.Insert(0, "0");
+
+        var operators = new List<string>() { "^", "/", "*", "-", "+" };
+
+        bool containsPower = range.Contains("^");
+        while (containsPower)
+        {
+            int indexOfPower = range.IndexOf("^");
+
+            double firstNum = double.Parse(range[indexOfPower - 1]);
+            double secondNum = GetSecondNum(range, indexOfPower);
+
+            double result = Math.Pow(firstNum, secondNum);
+
+            bool secondNumNegative = 0 > secondNum;
+            if (secondNumNegative)
+            {
+                range.RemoveAt(indexOfPower + 1);
+            }
+            range.RemoveAt(indexOfPower + 1);
+            range.RemoveAt(indexOfPower);
+            range.RemoveAt(indexOfPower - 1);
+
+            range.Insert(indexOfPower - 1, result.ToString());
+
+            containsPower = range.Contains("^");
+        }
+
+        bool containsDivision = range.Contains("/");
+        while (containsDivision)
+        {
+            int indexOfDivision = range.IndexOf("/");
+
+            double firstNum = double.Parse(range[indexOfDivision - 1]);
+            double secondNum = GetSecondNum(range, indexOfDivision);
+
+            double result = firstNum / secondNum;
+        }
+    }
+
+    public static double GetSecondNum(List<string> range, int index)
+    {
+        bool isNumber = double.TryParse(range[index + 1], out double num);
+        if (isNumber)
+        {
+            return num;
+        }
+        else
+        {
+            bool foundNegativeNumber = range[index + 1] == "-";
+            if (foundNegativeNumber)
+            {
+                num = double.Parse(range[index + 2]);
+                num = 0 - num;
+            }
+            return num;
+        }
     }
 }
