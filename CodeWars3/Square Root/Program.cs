dac4f2d670633beeb202f67e236054f35ff6c388
Uendy <alex.beshkov98@gmail.com>
2019-12-15 21:33:36 +0200

    Update: -Kata: Square Root

--- CodeWars3/Square Root/Program.cs
+++ CodeWars3/Square Root/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 public class Program
 {
     public static void Main()
@@ -10,7 +7,11 @@ public class Program
         bool isNegative = input < 0;
         input = Math.Abs(input);
 
-        double root = FindSquareRoot(input);
+        double startNum = 0;
+        double range = 1;
+        
+
+        double root = FindSquareRoot(input, startNum, range);
 
         if (isNegative)
         {
@@ -19,7 +20,7 @@ public class Program
         Console.WriteLine(root);
     }
 
-    public static double FindSquareRoot(double input)
+    public static double FindSquareRoot(double input, double startNum, double range)
     {
         // nestd for cycles where we do [index ^ (power of ) index]
         // until we get to a the input num or a num higher than input num
@@ -27,5 +28,24 @@ public class Program
         // go between the last num smaller than input and bigger than input and cycle through with 0.1 bigger precision
         // since nested it will do this 10 times before it stops and returns the closest answer
         // as some nums dont have a perfect square
+
+        for (double num = startNum; ; num += range)
+        {
+            double currentNum = Math.Pow(num, 2);
+
+            bool foundRoot = currentNum == input;
+            if (foundRoot)
+            {
+                return num;
+            }
+
+            bool overShot = currentNum > input;
+            if (overShot)
+            {
+                range /= 10;
+                double lastOkayNumber = currentNum - range;
+                FindSquareRoot(input, lastOkayNumber, range);
+            }
+        }
     }
 }
