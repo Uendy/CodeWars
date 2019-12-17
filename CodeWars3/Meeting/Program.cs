52c90d3898aca52dc19b6ec0d82d68267a3db406
Uendy <alex.beshkov98@gmail.com>
2019-12-17 11:41:47 +0200

    Adding: -Kata: Meeting (Initial)

---
+++ CodeWars3/Meeting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        //https://www.codewars.com/kata/meeting/train/csharp
+
+        //makes this string uppercase
+        //gives it sorted in alphabetical order by last name.
+        //When the last names are the same, sort them by first name.
+        //Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+        //Ex: 
+        //input = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+        //output = "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+
+    }
+}
