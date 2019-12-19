3a1e6a3b96a2df49d9310766cc58ad52ac862179
Uendy <alex.beshkov98@gmail.com>
2019-12-19 17:11:09 +0200

    Update: -Kata: Packing Your Backpack

--- CodeWars3/Packing Your Backpack/Program.cs
+++ CodeWars3/Packing Your Backpack/Program.cs
@@ -18,13 +18,34 @@ public class Program
         //You input will always be valid lists of equal length, so you don't have to worry about verifying your input.
 
         //You'll also be given a maximum weight. This is the weight that your backpack cannot exceed.
-        
+
         //Ex:
         //scores = [15, 10, 9, 5]
         //weights = [1, 5, 3, 4]
         //capacity = 8
         //The maximum score will be 29.This number comes from bringing items 1, 3 and 4.
-        
+
         //Note: Your solution will have to be efficient as the running time of your algorithm will be put to a test.
+
+        var scores = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        var weights = Console.ReadLine().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        int capacity = int.Parse(Console.ReadLine());
+
+        var luggage = new Dictionary<int, int>();
+        for (int index = 0; index < scores.Length; index++)
+        {
+            luggage[scores[index]] = weights[index];
+        }
+
+        luggage = luggage.OrderBy(x => x.Key).ThenBy(x => x.Value < capacity).ToDictionary(x => x.Key, y => y.Value);
+
+        var indexOfItems = new List<int>();
+        foreach (var key in luggage.Keys)
+        {
+
+        }
+        string outPut = string.Join(", ", indexOfItems);
+        Console.WriteLine(outPut);
     }
 }
