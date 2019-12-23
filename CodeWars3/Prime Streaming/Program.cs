8b16897c1a1b358997ec24117a1517ae945ab643
Uendy <alex.beshkov98@gmail.com>
2019-12-23 16:54:16 +0200

    Adding: -Prime Streaming (Initial)

---
+++ CodeWars3/Prime Streaming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Program
+{
+    public static void Main()
+    {
+        //https://www.codewars.com/kata/prime-streaming-pg-13/train/csharp
+
+        //Create an endless stream of prime numbers - a bit like IntStream.of(2, 3, 5, 7, 11, 13, 17), but infinite. 
+        //The stream must be able to produce a million primes in a few seconds.
+
+        //input1 = start from
+        //input2 = number of primes wanted
+
+        //ex
+        // 0, 10
+        // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+
+    }
+}
